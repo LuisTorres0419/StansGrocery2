61160e159e00bda5e16f8b2f2e5ed725eeff9c2f
luis torres <tl224627@gmail.com>
2020-11-05 08:51:18 -0700

    add clean  up

--- StansGroceryForm.Designer.vb
+++ StansGroceryForm.Designer.vb
@@ -22,8 +22,10 @@ Partial Class StansGroceryForm
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Me.TopMenuStrip = New System.Windows.Forms.MenuStrip()
         Me.FileTopMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.HelpTopMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.AboutTopMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.TextBox1 = New System.Windows.Forms.TextBox()
@@ -34,45 +36,57 @@ Partial Class StansGroceryForm
         Me.SearchButton = New System.Windows.Forms.Button()
         Me.LeftGroupBox = New System.Windows.Forms.GroupBox()
         Me.DisplayLabel = New System.Windows.Forms.Label()
+        Me.LookUpItem = New System.Windows.Forms.ToolTip(Me.components)
+        Me.ItemSelect = New System.Windows.Forms.ToolTip(Me.components)
         Me.TopMenuStrip.SuspendLayout()
         Me.LeftGroupBox.SuspendLayout()
         Me.SuspendLayout()
         '
         'TopMenuStrip
         '
+        Me.TopMenuStrip.GripMargin = New System.Windows.Forms.Padding(2, 2, 0, 2)
+        Me.TopMenuStrip.ImageScalingSize = New System.Drawing.Size(32, 32)
         Me.TopMenuStrip.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileTopMenuItem, Me.HelpTopMenuItem})
         Me.TopMenuStrip.Location = New System.Drawing.Point(0, 0)
         Me.TopMenuStrip.Name = "TopMenuStrip"
-        Me.TopMenuStrip.Size = New System.Drawing.Size(625, 24)
+        Me.TopMenuStrip.Size = New System.Drawing.Size(1250, 40)
         Me.TopMenuStrip.TabIndex = 0
         Me.TopMenuStrip.Text = "MenuStrip1"
         '
         'FileTopMenuItem
         '
+        Me.FileTopMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ExitToolStripMenuItem})
         Me.FileTopMenuItem.Name = "FileTopMenuItem"
-        Me.FileTopMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.FileTopMenuItem.Size = New System.Drawing.Size(72, 38)
         Me.FileTopMenuItem.Text = "&File"
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(359, 44)
+        Me.ExitToolStripMenuItem.Text = "Exit"
         '
         'HelpTopMenuItem
         '
         Me.HelpTopMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AboutTopMenuItem})
         Me.HelpTopMenuItem.Name = "HelpTopMenuItem"
-        Me.HelpTopMenuItem.Size = New System.Drawing.Size(44, 20)
+        Me.HelpTopMenuItem.Size = New System.Drawing.Size(85, 38)
         Me.HelpTopMenuItem.Text = "Help"
         '
         'AboutTopMenuItem
         '
         Me.AboutTopMenuItem.Name = "AboutTopMenuItem"
-        Me.AboutTopMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.AboutTopMenuItem.Size = New System.Drawing.Size(214, 44)
         Me.AboutTopMenuItem.Text = "About"
         '
         'TextBox1
         '
         Me.TextBox1.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Append
         Me.TextBox1.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.AllSystemSources
-        Me.TextBox1.Location = New System.Drawing.Point(82, 46)
+        Me.TextBox1.Location = New System.Drawing.Point(164, 88)
+        Me.TextBox1.Margin = New System.Windows.Forms.Padding(6)
         Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(411, 20)
+        Me.TextBox1.Size = New System.Drawing.Size(818, 31)
         Me.TextBox1.TabIndex = 1
         '
         'ComboBox1
@@ -80,42 +94,48 @@ Partial Class StansGroceryForm
         Me.ComboBox1.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Append
         Me.ComboBox1.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems
         Me.ComboBox1.FormattingEnabled = True
-        Me.ComboBox1.Location = New System.Drawing.Point(82, 72)
+        Me.ComboBox1.Location = New System.Drawing.Point(164, 138)
+        Me.ComboBox1.Margin = New System.Windows.Forms.Padding(6)
         Me.ComboBox1.Name = "ComboBox1"
-        Me.ComboBox1.Size = New System.Drawing.Size(218, 21)
+        Me.ComboBox1.Size = New System.Drawing.Size(432, 33)
         Me.ComboBox1.TabIndex = 2
         '
         'DisplayListBox
         '
         Me.DisplayListBox.FormattingEnabled = True
-        Me.DisplayListBox.Location = New System.Drawing.Point(306, 72)
+        Me.DisplayListBox.ItemHeight = 25
+        Me.DisplayListBox.Location = New System.Drawing.Point(612, 138)
+        Me.DisplayListBox.Margin = New System.Windows.Forms.Padding(6)
         Me.DisplayListBox.Name = "DisplayListBox"
-        Me.DisplayListBox.Size = New System.Drawing.Size(291, 251)
+        Me.DisplayListBox.Size = New System.Drawing.Size(578, 479)
         Me.DisplayListBox.TabIndex = 3
         '
         'LookUpLabel
         '
         Me.LookUpLabel.AutoSize = True
-        Me.LookUpLabel.Location = New System.Drawing.Point(5, 46)
+        Me.LookUpLabel.Location = New System.Drawing.Point(10, 88)
+        Me.LookUpLabel.Margin = New System.Windows.Forms.Padding(6, 0, 6, 0)
         Me.LookUpLabel.Name = "LookUpLabel"
-        Me.LookUpLabel.Size = New System.Drawing.Size(71, 13)
+        Me.LookUpLabel.Size = New System.Drawing.Size(138, 25)
         Me.LookUpLabel.TabIndex = 4
         Me.LookUpLabel.Text = "Look Up Item"
         '
         'SelectLabel
         '
         Me.SelectLabel.AutoSize = True
-        Me.SelectLabel.Location = New System.Drawing.Point(16, 72)
+        Me.SelectLabel.Location = New System.Drawing.Point(32, 138)
+        Me.SelectLabel.Margin = New System.Windows.Forms.Padding(6, 0, 6, 0)
         Me.SelectLabel.Name = "SelectLabel"
-        Me.SelectLabel.Size = New System.Drawing.Size(60, 13)
+        Me.SelectLabel.Size = New System.Drawing.Size(118, 25)
         Me.SelectLabel.TabIndex = 5
         Me.SelectLabel.Text = "Select Item"
         '
         'SearchButton
         '
-        Me.SearchButton.Location = New System.Drawing.Point(499, 46)
+        Me.SearchButton.Location = New System.Drawing.Point(998, 88)
+        Me.SearchButton.Margin = New System.Windows.Forms.Padding(6)
         Me.SearchButton.Name = "SearchButton"
-        Me.SearchButton.Size = New System.Drawing.Size(98, 20)
+        Me.SearchButton.Size = New System.Drawing.Size(196, 38)
         Me.SearchButton.TabIndex = 6
         Me.SearchButton.Text = "&Search"
         Me.SearchButton.UseVisualStyleBackColor = True
@@ -123,24 +143,27 @@ Partial Class StansGroceryForm
         'LeftGroupBox
         '
         Me.LeftGroupBox.Controls.Add(Me.DisplayLabel)
-        Me.LeftGroupBox.Location = New System.Drawing.Point(82, 99)
+        Me.LeftGroupBox.Location = New System.Drawing.Point(164, 190)
+        Me.LeftGroupBox.Margin = New System.Windows.Forms.Padding(6)
         Me.LeftGroupBox.Name = "LeftGroupBox"
-        Me.LeftGroupBox.Size = New System.Drawing.Size(218, 224)
+        Me.LeftGroupBox.Padding = New System.Windows.Forms.Padding(6)
+        Me.LeftGroupBox.Size = New System.Drawing.Size(436, 431)
         Me.LeftGroupBox.TabIndex = 7
         Me.LeftGroupBox.TabStop = False
         '
         'DisplayLabel
         '
-        Me.DisplayLabel.Location = New System.Drawing.Point(3, 16)
+        Me.DisplayLabel.Location = New System.Drawing.Point(6, 31)
+        Me.DisplayLabel.Margin = New System.Windows.Forms.Padding(6, 0, 6, 0)
         Me.DisplayLabel.Name = "DisplayLabel"
-        Me.DisplayLabel.Size = New System.Drawing.Size(209, 205)
+        Me.DisplayLabel.Size = New System.Drawing.Size(418, 394)
         Me.DisplayLabel.TabIndex = 0
         '
         'StansGroceryForm
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 25.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(625, 347)
+        Me.ClientSize = New System.Drawing.Size(1250, 667)
         Me.Controls.Add(Me.LeftGroupBox)
         Me.Controls.Add(Me.SearchButton)
         Me.Controls.Add(Me.SelectLabel)
@@ -150,9 +173,12 @@ Partial Class StansGroceryForm
         Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.TopMenuStrip)
         Me.MainMenuStrip = Me.TopMenuStrip
+        Me.Margin = New System.Windows.Forms.Padding(6)
         Me.Name = "StansGroceryForm"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Welcome to Stan's Grocery"
+        Me.LookUpItem.SetToolTip(Me, "Type The Item Here")
+        Me.ItemSelect.SetToolTip(Me, "Select Item Here")
         Me.TopMenuStrip.ResumeLayout(False)
         Me.TopMenuStrip.PerformLayout()
         Me.LeftGroupBox.ResumeLayout(False)
@@ -173,4 +199,7 @@ Partial Class StansGroceryForm
     Friend WithEvents SearchButton As Button
     Friend WithEvents LeftGroupBox As GroupBox
     Friend WithEvents DisplayLabel As Label
+    Friend WithEvents LookUpItem As ToolTip
+    Friend WithEvents ItemSelect As ToolTip
+    Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
 End Class
--- StansGroceryForm.vb
+++ StansGroceryForm.vb
@@ -1,3 +1,62 @@
 ﻿Public Class StansGroceryForm
+    Dim food(255, 2) As String
+
+    Sub LoadDataFile()
+        Dim temp() As String
+        temp = Split(My.Resources.Grocery, vbNewLine)
+
+        Dim secondArry() As String
+        Dim thirdArray() As String
+
+
+        For Each foods In temp
+
+            secondArry = Split(foods, $"{Chr(34)},{Chr(34)}")
+            thirdArray = Split(secondArry(0), "$$TM")
+            Console.WriteLine(thirdArray(1))
+            thirdArray = Split(secondArry(1), "##LOC")
+            Console.WriteLine(thirdArray(1))
+            thirdArray = Split(secondArry(2), "%%CAT")
+            thirdArray = Split(thirdArray(1), Chr(34))
+            Console.WriteLine(thirdArray(0))
+
+
+        Next
+
+    End Sub
+
+    Private Sub StansGroceryForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Sub Tester()
+        Dim tempData() As String
+
+        Dim fileName As String
+        'tempData = My.Resources.Grocery.ToArray
+        fileName = "..\..\Resources\Grocery.txt"
+
+
+    End Sub
+
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        Me.Close()
+    End Sub
+
+    Private Sub DisplayListBox_SelectedIndexChanged(sender As Object, e As EventArgs) Handles DisplayListBox.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub SearchButton_Click(sender As Object, e As EventArgs) Handles SearchButton.Click
+
+    End Sub
 
 End Class
